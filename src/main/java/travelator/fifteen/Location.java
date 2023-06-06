490bb1da9af35ea6c77e4104563a3f57391400d5
JungwooSim <cscd053@gmail.com>
2023-06-06 15:16:11 +0900

    15장. 캡슐화한 컬렉션에서 타입 별명으로

---
+++ src/main/java/travelator/fifteen/Location.java
@@ -0,0 +1,4 @@
+package travelator.fifteen;
+
+public class Location {
+}
---
+++ src/main/java/travelator/fifteen/TravelMethod.java
@@ -0,0 +1,11 @@
+package travelator.fifteen;
+
+public enum TravelMethod {
+    AIR,
+    SEA,
+    RAIL,
+    BUS,
+    CAR,
+    CARRIAGE,
+    CAMEL;
+}
